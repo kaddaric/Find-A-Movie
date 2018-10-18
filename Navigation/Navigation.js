0a6a75c25677af2af1f02cd2a392dd2db2791e91
kaddaric <f.cedric.f@gmail.com>
2018-10-18 23:33:46 +0200

    creation d'une barre de navigation

--- Navigation/Navigation.js
+++ Navigation/Navigation.js
@@ -1,6 +1,9 @@
-import { createStackNavigator } from 'react-navigation';
+import React from 'react';
+import { createStackNavigator, createBottomTabNavigator } from 'react-navigation';
+import { StyleSheet, Image } from 'react-native';
 import Search from '../components/Search';
 import FilmDetail from '../components/FilmDetail';
+import Favorites from '../components/Favorites';
 
 const SearchStackNavigator = createStackNavigator({
   Search: {
@@ -14,4 +17,44 @@ const SearchStackNavigator = createStackNavigator({
   }
 })
 
-export default SearchStackNavigator;
+const MoviesTabNavigator = createBottomTabNavigator(
+  {
+    Search: {
+      screen: SearchStackNavigator,
+      navigationOptions: {
+        tabBarIcon: () => {
+          return <Image
+              source={require('../images/ic_search.png')}
+              style={styles.icon} />
+        }
+      },
+    },
+    Favorites: {
+      screen: Favorites,
+      navigationOptions: {
+        tabBarIcon: () => {
+          return <Image
+            source={require('../images/ic_favorite.png')}
+            style={styles.icon} />
+        }
+      }
+    },
+  },
+  {
+    tabBarOptions: {
+      activeBackgroundColor: '#DDDDDD', // Couleur d'arrière-plan de l'onglet sélectionné
+      inactiveBackgroundColor: '#FFFFFF', // Couleur d'arrière-plan des onglets non sélectionnés
+      showLabel: false, // On masque les titres
+      showIcon: true // On informe le TabNavigator qu'on souhaite afficher les icônes définis
+    }
+  }
+)
+
+const styles = StyleSheet.create({
+  icon: {
+    width: 30,
+    height: 30
+  }
+})
+
+export default MoviesTabNavigator;
